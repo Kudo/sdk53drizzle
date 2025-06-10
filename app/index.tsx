1dcae36ee61e030fd5c763685a426695e6490bce
Kudo Chien <kudo@expo.dev>
2025-06-10 23:43:05 +0800

    add drizzle
    referenced from https://expo.dev/blog/modern-sqlite-for-react-native-apps

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
+import * as schema from '@/db/schema';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import { useSQLiteContext } from 'expo-sqlite';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db, { schema });
+
+  // more code
 }
