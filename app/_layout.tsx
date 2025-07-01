c653e00d2491a2b0cac03eda083917c0698d2bb2
Kudo Chien <kudo@expo.dev>
2025-07-01 10:00:44 +0800

    add tab layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,75 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { SQLiteProvider } from 'expo-sqlite';
-import { Suspense } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { Suspense, useEffect } from 'react';
 import { ActivityIndicator } from 'react-native';
+import 'react-native-reanimated';
 
+import { useColorScheme } from '@/components/useColorScheme';
 import { DrizzleContextProvider, migrateAsync } from '@/hooks/useDrizzle';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+};
 
 export const DATABASE_NAME = 'tasks';
 
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Suspense fallback={<ActivityIndicator size="large" />}>
-      <SQLiteProvider
-        databaseName={DATABASE_NAME}
-        options={{ enableChangeListener: true }}
-        useSuspense
-        onInit={migrateAsync}
-      >
-        <DrizzleContextProvider>
-          <Stack>
-            <Stack.Screen name="index" options={{ title: 'Tasks' }} />
-          </Stack>
-        </DrizzleContextProvider>
-      </SQLiteProvider>
-    </Suspense>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Suspense fallback={<ActivityIndicator size="large" />}>
+        <SQLiteProvider
+          databaseName={DATABASE_NAME}
+          options={{ enableChangeListener: true }}
+          useSuspense
+          onInit={migrateAsync}
+        >
+          <DrizzleContextProvider>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+            </Stack>
+          </DrizzleContextProvider>
+        </SQLiteProvider>
+      </Suspense>
+    </ThemeProvider>
   );
 }
